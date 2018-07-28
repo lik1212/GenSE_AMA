9d4d50fa85c15142bc9acae27db36c3ea183f276
brandalik <brandalik@eit.uni-kl.de>
2018-07-28 17:07:23 +0200

    Add files and functions to be able to compare SE results with "perfect" load flow results.

---
+++ Comparison_Functions/NodeRes2BranchRes.m
@@ -0,0 +1,102 @@
+function BranchRes = NodeRes2BranchRes(NodeRes, SinInfo, Y_L1L2L3)
+%NodeRes2BranchRes - Sincal NodeRes to BranchRes, Y_L1L2L3 (admittance
+%matrix in conductor quantity) can be get with the functions GetLineInfo,
+%LineInfo2Y_012 & Y_012_to_Y_L1L2L3
+%
+% Author(s): P.Gassler, R. Brandalik 
+
+%% Initial preperation
+
+num_Nodes       = numel(SinInfo.Node.Node_ID   );
+num_Lines       = numel(SinInfo.Line.Element_ID);
+num_instants    = numel(NodeRes.ResTime)/num_Nodes;
+BranchRes_12    = zeros(num_instants * num_Lines,6); % Initial branches in one direction
+BranchRes_21    = zeros(num_instants * num_Lines,6); % Initial branches in the other direction
+SinInfo.Line    = sortrows(SinInfo.Line, 'Element_ID'); % Sort Lines by Element_ID, TODO: Needed?
+SinInfo.Node    = sortrows(SinInfo.Node, 'Node_ID'   ); % Sord Nodes by Node_ID   , TODO: Needed?
+NodeRes.U1_comp = NodeRes.U1 .* exp(1i * deg2rad(NodeRes.phi1)); % comp - Complex voltage
+NodeRes.U2_comp = NodeRes.U2 .* exp(1i * deg2rad(NodeRes.phi2));
+NodeRes.U3_comp = NodeRes.U3 .* exp(1i * deg2rad(NodeRes.phi3));
+
+%% NodeRes to BranchRes
+
+for k_Line = 1 : num_Lines % over all lines
+    Node1_ID  = SinInfo.Line.Node1_ID(k_Line); % Node1_ID of Line
+    Node2_ID  = SinInfo.Line.Node2_ID(k_Line); % Node2_ID of Line
+    Node1_Num = find(ismember(SinInfo.Node.Node_ID, Node1_ID));
+    Node2_Num = find(ismember(SinInfo.Node.Node_ID, Node2_ID));
+    k_U1 = reshape([...
+        NodeRes.U1_comp(NodeRes.Node_ID == Node1_ID),...
+        NodeRes.U2_comp(NodeRes.Node_ID == Node1_ID),...
+        NodeRes.U3_comp(NodeRes.Node_ID == Node1_ID) ...
+        ].',[],1); % Voltage one Node of Line
+    k_U2 = reshape([...
+        NodeRes.U1_comp(NodeRes.Node_ID == Node2_ID),...
+        NodeRes.U2_comp(NodeRes.Node_ID == Node2_ID),...
+        NodeRes.U3_comp(NodeRes.Node_ID == Node2_ID) ...
+        ].',[],1); % Voltage other Node of Line
+    U12 = k_U1 - k_U2; % Voltage difference in one direction
+    U21 = k_U2 - k_U1; % Voltage difference in the other direction
+    k_U1_NodeOrder = [...
+        NodeRes.U1_comp(NodeRes.Node_ID == Node1_ID);...
+        NodeRes.U2_comp(NodeRes.Node_ID == Node1_ID);...
+        NodeRes.U3_comp(NodeRes.Node_ID == Node1_ID) ...
+        ];
+    k_U2_NodeOrder = [...
+        NodeRes.U1_comp(NodeRes.Node_ID == Node2_ID);...
+        NodeRes.U2_comp(NodeRes.Node_ID == Node2_ID);...
+        NodeRes.U3_comp(NodeRes.Node_ID == Node2_ID) ...
+        ];    
+    Y_k_Line = Y_L1L2L3(...
+        (Node1_Num - 1) * 3 + 1 : (Node1_Num - 1) * 3 + 3, ...
+        (Node2_Num - 1) * 3 + 1 : (Node2_Num - 1) * 3 + 3); % Line admittance
+    % Currents in both directions
+    I12(:,1) = sum(reshape(repmat(Y_k_Line,num_instants,1) .* U12,3,[])); % Order is: [L1 all instances, L2 all instances, L3 ...]
+    I21(:,1) = sum(reshape(repmat(Y_k_Line,num_instants,1) .* U21,3,[])); % Order is: [L1 all instances, L2 all instances, L3 ...]
+    S12 = k_U1_NodeOrder .* conj(I12);
+    S21 = k_U2_NodeOrder .* conj(I21);
+    % One diretcion
+    BranchRes_12((k_Line - 1) * num_instants + 1 : k_Line * num_instants, 1) = repmat(SinInfo.Line.Terminal1_ID(k_Line),num_instants,1); % Terminal1_ID
+    BranchRes_12((k_Line - 1) * num_instants + 1 : k_Line * num_instants, 2) = repmat(SinInfo.Line.Terminal2_ID(k_Line),num_instants,1); % Terminal2_ID
+    BranchRes_12((k_Line - 1) * num_instants + 1 : k_Line * num_instants, 3) = real(S12(                   1 : num_instants    ));       % P1
+    BranchRes_12((k_Line - 1) * num_instants + 1 : k_Line * num_instants, 4) = imag(S12(                   1 : num_instants    ));       % Q1
+    BranchRes_12((k_Line - 1) * num_instants + 1 : k_Line * num_instants, 5) = abs (S12(                   1 : num_instants    ));       % S1    
+    BranchRes_12((k_Line - 1) * num_instants + 1 : k_Line * num_instants, 6) = abs (I12(                   1 : num_instants    ));       % I1
+    BranchRes_12((k_Line - 1) * num_instants + 1 : k_Line * num_instants, 7) = real(S12(    num_instants + 1 : num_instants * 2));       % P2
+    BranchRes_12((k_Line - 1) * num_instants + 1 : k_Line * num_instants, 8) = imag(S12(    num_instants + 1 : num_instants * 2));       % Q2
+    BranchRes_12((k_Line - 1) * num_instants + 1 : k_Line * num_instants, 9) = abs (S12(    num_instants + 1 : num_instants * 2));       % S2    
+    BranchRes_12((k_Line - 1) * num_instants + 1 : k_Line * num_instants,10) = abs (I12(    num_instants + 1 : num_instants * 2));       % I2
+    BranchRes_12((k_Line - 1) * num_instants + 1 : k_Line * num_instants,11) = real(S12(2 * num_instants + 1 : num_instants * 3));       % P3
+    BranchRes_12((k_Line - 1) * num_instants + 1 : k_Line * num_instants,12) = imag(S12(2 * num_instants + 1 : num_instants * 3));       % Q3
+    BranchRes_12((k_Line - 1) * num_instants + 1 : k_Line * num_instants,13) = abs (S12(2 * num_instants + 1 : num_instants * 3));       % S3    
+    BranchRes_12((k_Line - 1) * num_instants + 1 : k_Line * num_instants,14) = abs (I12(2 * num_instants + 1 : num_instants * 3));       % I3
+    BranchRes_12((k_Line - 1) * num_instants + 1 : k_Line * num_instants,15) = 1 : num_instants;                                         % ResTime
+    % Other direction
+    BranchRes_21((k_Line - 1) * num_instants + 1 : k_Line * num_instants, 1) = repmat(SinInfo.Line.Terminal2_ID(k_Line),num_instants,1); % Terminal1_ID
+    BranchRes_21((k_Line - 1) * num_instants + 1 : k_Line * num_instants, 2) = repmat(SinInfo.Line.Terminal1_ID(k_Line),num_instants,1); % Terminal2_ID
+    BranchRes_21((k_Line - 1) * num_instants + 1 : k_Line * num_instants, 3) = real(S21(                   1 : num_instants    ));       % P1
+    BranchRes_21((k_Line - 1) * num_instants + 1 : k_Line * num_instants, 4) = imag(S21(                   1 : num_instants    ));       % Q1
+    BranchRes_21((k_Line - 1) * num_instants + 1 : k_Line * num_instants, 5) = abs (S21(                   1 : num_instants    ));       % S1
+    BranchRes_21((k_Line - 1) * num_instants + 1 : k_Line * num_instants, 6) = abs (I21(                   1 : num_instants    ));       % I1
+    BranchRes_21((k_Line - 1) * num_instants + 1 : k_Line * num_instants, 7) = real(S21(    num_instants + 1 : num_instants * 2));       % P2
+    BranchRes_21((k_Line - 1) * num_instants + 1 : k_Line * num_instants, 8) = imag(S21(    num_instants + 1 : num_instants * 2));       % Q2
+    BranchRes_21((k_Line - 1) * num_instants + 1 : k_Line * num_instants, 9) = abs (S21(    num_instants + 1 : num_instants * 2));       % S2
+    BranchRes_21((k_Line - 1) * num_instants + 1 : k_Line * num_instants,10) = abs (I21(    num_instants + 1 : num_instants * 2));       % I2
+    BranchRes_21((k_Line - 1) * num_instants + 1 : k_Line * num_instants,11) = real(S21(2 * num_instants + 1 : num_instants * 3));       % P3
+    BranchRes_21((k_Line - 1) * num_instants + 1 : k_Line * num_instants,12) = imag(S21(2 * num_instants + 1 : num_instants * 3));       % Q3
+    BranchRes_21((k_Line - 1) * num_instants + 1 : k_Line * num_instants,13) = abs (S21(2 * num_instants + 1 : num_instants * 3));       % S3    
+    BranchRes_21((k_Line - 1) * num_instants + 1 : k_Line * num_instants,14) = abs (I21(2 * num_instants + 1 : num_instants * 3));       % I3  
+    BranchRes_21((k_Line - 1) * num_instants + 1 : k_Line * num_instants,15) = 1 : num_instants;                                         % ResTime
+end
+
+clear NodeRes
+BranchRes = [BranchRes_12; BranchRes_21];
+clear BranchRes_12 BranchRes21
+BranchRes = array2table(BranchRes, 'VariableNames', {'Terminal1_ID','Terminal2_ID',...
+    'P1','Q1','S1','I1', ...
+    'P2','Q2','S2','I2', ...
+    'P3','Q3','S3','I3', ...
+    'ResTime'});
+
+BranchRes = sortrows(BranchRes, 'Terminal1_ID', 'ascend');
+BranchRes = sortrows(BranchRes, 'ResTime',      'ascend');
---
+++ Comparison_Functions/z_full2NodeRes_all.m
@@ -0,0 +1,83 @@
+function NodeRes_all_estim = z_full2NodeRes_all(z_hat_full, SinInfo)
+%Z_FULL2NODERES_ALL Modify the SE results in z_hat_full to match the
+%NodeRes form (Sincal results)
+%
+%   Input:
+%       z_hat_full - Result of State Estimation (U, phi, P and Q for all
+%                    Nodes)
+%       SinInfo    - Sincal Grid Information
+%
+%   Output:
+%       NodeRes_all_estim - State Estimation results in the same form as
+%                           the Sincal load flow results
+%
+% Author(s): P.Gassler, R. Brandalik 
+
+%% Initial prepeartion
+
+num_meas     = size(z_hat_full, 1);
+num_instants = size(z_hat_full, 2);
+Node_IDs     = sort(SinInfo.Node.Node_ID);
+num_Nodes    = numel(Node_IDs);
+
+%% Preperation of measurement position
+
+% Initial
+U1_pos   = false(num_meas, 1);
+U2_pos   = false(num_meas, 1);
+U3_pos   = false(num_meas, 1);
+phi1_pos = false(num_meas, 1);
+phi2_pos = false(num_meas, 1);
+phi3_pos = false(num_meas, 1);
+P1_pos   = false(num_meas, 1);
+P2_pos   = false(num_meas, 1);
+P3_pos   = false(num_meas, 1);
+Q1_pos   = false(num_meas, 1);
+Q2_pos   = false(num_meas, 1);
+Q3_pos   = false(num_meas, 1);
+
+schema_L1 = repmat(logical([1; 0; 0]), num_Nodes, 1);
+schema_L2 = repmat(logical([0; 1; 0]), num_Nodes, 1);
+schema_L3 = repmat(logical([0; 0; 1]), num_Nodes, 1);
+
+U1_pos  (                   1 : 3 *num_Nodes    ) = schema_L1;
+U2_pos  (                   1 : 3 *num_Nodes    ) = schema_L2;
+U3_pos  (                   1 : 3 *num_Nodes    ) = schema_L3;
+phi1_pos(3 *num_Nodes +     1 : 3 *num_Nodes * 2) = schema_L1;
+phi2_pos(3 *num_Nodes +     1 : 3 *num_Nodes * 2) = schema_L2;
+phi3_pos(3 *num_Nodes +     1 : 3 *num_Nodes * 2) = schema_L3;
+P1_pos  (3 *num_Nodes * 2 + 1 : 3 *num_Nodes * 3) = schema_L1;
+P2_pos  (3 *num_Nodes * 2 + 1 : 3 *num_Nodes * 3) = schema_L2;
+P3_pos  (3 *num_Nodes * 2 + 1 : 3 *num_Nodes * 3) = schema_L3;
+Q1_pos  (3 *num_Nodes * 3 + 1 : 3 *num_Nodes * 4) = schema_L1;
+Q2_pos  (3 *num_Nodes * 3 + 1 : 3 *num_Nodes * 4) = schema_L2;
+Q3_pos  (3 *num_Nodes * 3 + 1 : 3 *num_Nodes * 4) = schema_L3;
+
+%% Build NodeRes_all_estim
+
+NodeRes_all_estim = zeros(num_Nodes * num_instants,19); % Initial
+
+NodeRes_all_estim(:, 1) = repmat(Node_IDs, num_instants, 1);                              % Node_ID
+NodeRes_all_estim(:, 2) =         reshape(z_hat_full(U1_pos  ,:),[], 1) * 10^-3;          % U1
+NodeRes_all_estim(:, 7) =         reshape(z_hat_full(U2_pos  ,:),[], 1) * 10^-3;          % U2
+NodeRes_all_estim(:,12) =         reshape(z_hat_full(U3_pos  ,:),[], 1) * 10^-3;          % U3
+NodeRes_all_estim(:, 3) = rad2deg(reshape(z_hat_full(phi1_pos,:),[], 1));                 % phi1
+NodeRes_all_estim(:, 8) = rad2deg(reshape(z_hat_full(phi2_pos,:),[], 1));                 % phi2
+NodeRes_all_estim(:,13) = rad2deg(reshape(z_hat_full(phi3_pos,:),[], 1));                 % phi3
+NodeRes_all_estim(:, 4) =         reshape(z_hat_full(P1_pos  ,:),[], 1) * 10^-6;          % P1
+NodeRes_all_estim(:, 9) =         reshape(z_hat_full(P2_pos  ,:),[], 1) * 10^-6;          % P2
+NodeRes_all_estim(:,14) =         reshape(z_hat_full(P3_pos  ,:),[], 1) * 10^-6;          % P3
+NodeRes_all_estim(:, 5) =         reshape(z_hat_full(Q1_pos  ,:),[], 1) * 10^-6;          % Q1
+NodeRes_all_estim(:,10) =         reshape(z_hat_full(Q2_pos  ,:),[], 1) * 10^-6;          % Q2
+NodeRes_all_estim(:,15) =         reshape(z_hat_full(Q3_pos  ,:),[], 1) * 10^-6;          % Q3
+NodeRes_all_estim(:, 6) = sqrt(NodeRes_all_estim(:, 5).^2 + NodeRes_all_estim(:, 4).^2 ); % S1
+NodeRes_all_estim(:,11) = sqrt(NodeRes_all_estim(:,10).^2 + NodeRes_all_estim(:, 9).^2 ); % S2
+NodeRes_all_estim(:,16) = sqrt(NodeRes_all_estim(:,15).^2 + NodeRes_all_estim(:,14).^2 ); % S3
+NodeRes_all_estim(:,19) = repelem(1:num_instants, 1, num_Nodes);                          % ResTime
+
+NodeRes_all_estim = array2table(NodeRes_all_estim, 'VariableNames', ... % To table
+    {'Node_ID',...
+    'U1','phi1','P1','Q1','S1',...
+    'U2','phi2','P2','Q2','S2',...
+    'U3','phi3','P3','Q3','S3',...
+    'Ue','S','ResTime'});
